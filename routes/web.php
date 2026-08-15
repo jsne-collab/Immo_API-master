ce0ea43185f6269b56910a703e73be3210dbea67
josiane BEDRA <josianebedra@gmail.com>
2026-08-15 07:42:20 +0000

    Ajout route /fix pour artisan

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/fix', function () {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Caches vidés et migrations exécutées ✅';
+});
